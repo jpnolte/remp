faedf182b63035cdd66db45439cd015900c58099
JP JP <j-phnolte@web.de>
2023-01-03 16:02:25 +0100

    bootstrap fix

---
+++ R/demogr.R
@@ -0,0 +1,12 @@
+#' demogr
+#'
+#' TBA
+#' @format Ein tibble mit 4 Zeilen and 3 Spalten:
+#' \describe{
+#'   \item{ID}{TeilnehmerInnen Identifikator.}
+#'   \item{Sex}{Biologisches Geschlecht (Auswahl).}
+#'   \item{Alter}{Lebensalter in Jahren.}
+#' }
+#' @examples
+#' demogr
+"demogr"
---
+++ R/df_oben.R
@@ -0,0 +1,13 @@
+#' df_oben
+#'
+#' TBA
+#' @format Ein tibble mit 3 Zeilen and 4 Spalten:
+#' \describe{
+#'   \item{ID}{TeilnehmerInnen Identifikator.}
+#'   \item{Puls}{Anzahl an Schlägen pro Minute.}
+#'   \item{Blasenstoerung}{Es liegt eine Blasenstörung vor (1) oder nicht (0).}
+#'   \item{Infekt_2j}{Anzahl an Infektionen innerhalb von 2 Jahren.}
+#' }
+#' @examples
+#' df_oben
+"df_oben"

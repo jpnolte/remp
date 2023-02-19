ae003831eeb58fbcc0fd344e0fa4baaa1debb740
JP JP <j-phnolte@web.de>
2023-02-19 11:40:05 +0100

    minor changes6

--- R/big5.R
+++ R/big5.R
@@ -1,13 +1,18 @@
-#' Big 5 Modifikation
+#' BIG 5 Modifikation
 #'
-#' Dies ist die Nutzerstatistik eines User beim Film- und Serienschauen auf einer der bekanntesten Videostreaming Plattformen.
-#' @format Ein tibble mit 1,493 Zeilen and 4 Spalten:
+#' Der Datensatz enthält die soziodemographische Variablen Alter und Geschlecht, die durchschnittliche Ausprägung von Extraversion,
+#' Neurotizismus, Verträglichkeit und Gewissenhaftigkeit sowie 10 Items, um die Offenheit zu messen.
+#'
+#' @format Ein tible mit 200 Zeilen and 16 Spalten:
 #' \describe{
-#'   \item{Titel}{Names des Filmes oder der Serie}
-#'   \item{Staffel}{Staffelnummer}
-#'   \item{Folge}{Name der Folge}
-#'   \item{Watchdate}{Datum, an dem der Titel geschaut wurde. Beachte, dass aus didaktischen Gründen bewusst das falsche Datenformat vorliegt.}
+#'   \item{Alter}{Alter in Lebensjahren}
+#'   \item{Geschlecht}{f = Weiblich, m = Männlich}
+#'   \item{Extraversion}{Durchschnittliche Ausprägung von Extraversion}
+#'   \item{Neurotizismus}{Durchschnittliche Ausprägung von Neurotizismus}
+#'   \item{O1-O3}{Item 1-3 zur Ausprägung von Offenheit}
 #' }
 #' @examples
 #' big5
+#' 
+#' @source \url{https://openpsychometrics.org/_rawdata/} (modifiziert)
 "big5"
---
+++ R/big5_long.R
@@ -0,0 +1,19 @@
+#' BIG 5 im langen Format
+#'
+#' Der Datensatz enthält einen Teil des Big5 Datensatzes im langen Format.
+#'
+#' @format Ein tible mit 200 Zeilen and 16 Spalten:
+#' \describe{
+#'   \item{Alter}{Alter in Lebensjahren}
+#'   \item{Geschlecht}{f = Weiblich, m = Männlich}
+#'   \item{Gruppe}{Altersgruppe unterteilt in Jung, Mittel, Weise}
+#'   \item{ID}{Individuelle Personenidentifikation}
+#'   \item{Faktor}{Persönlichkeitsfaktor Extraversion oder Neurotizismus}
+#'   \item{Auspraegung}{Mittlere Ausprägung des jeweiligen Persönlichkeitsfaktors}
+#'   \item{Zeitpunkt}{Messzeitpunkt 1 oder 2}
+#' }
+#' @examples
+#' big5_long
+#' 
+#' @source \url{https://openpsychometrics.org/_rawdata/} (modifiziert)
+"big5_long"
